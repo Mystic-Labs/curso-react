417eff3317870a78f4c460fad885e35484c3fe70
arthur mario <artmario@gmail.com>
2020-03-02 13:48:58 -0400

    aula 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
+  const autores =[
+    {
+      nome:'paulo',
+      livro:'react',
+      preco:'10'
+    },
+    {
+      nome:'daniel',
+      livro:'java',
+      preco:'1000'
+    },
+    {
+      nome:'Marcos',
+      livro:'design',
+      preco:'150'
+    },
+    {
+      nome:'Bruno',
+      livro:'DevOps',
+      preco:'100'
+    },
+    {
+      nome:'vish',
+      livro:'nada',
+      preco:'999'
+    }
+  ];
   return (
     <div className="App">
-      <h1>Hello World</h1>
+    <Tabela autores = { autores } />
     </div>
   );
 }
